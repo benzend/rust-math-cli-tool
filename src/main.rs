ddc856e54cc9f5c845ae573b78af42d80d40764e
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2022-10-16 18:15:30 -0600

    chore: add tests
    becoming more tdd driven to help solve for Order of Ops

--- src/main.rs
+++ src/main.rs
@@ -22,30 +22,30 @@ enum Commands {
     #[clap(arg_required_else_help = true)]
     Add {
         /// first int
-        first_arg: u32,
+        first_arg: i32,
         /// second int
-        second_arg: u32,
+        second_arg: i32,
     },
     #[clap(arg_required_else_help = true)]
     Subtract {
         /// first int
-        first_arg: u32,
+        first_arg: i32,
         /// second int
-        second_arg: u32,
+        second_arg: i32,
     },
     #[clap(arg_required_else_help = true)]
     Multiply {
         /// first int
-        first_arg: u32,
+        first_arg: i32,
         /// second int
-        second_arg: u32,
+        second_arg: i32,
     },
     #[clap(arg_required_else_help = true)]
     Divide {
         /// first int
-        first_arg: u32,
+        first_arg: i32,
         /// second int
-        second_arg: u32,
+        second_arg: i32,
     },
 }
 
@@ -60,7 +60,7 @@ enum Operator {
 #[derive(Debug, PartialEq)]
 enum MathsArg {
     Op(Operator),
-    Int(u32),
+    Int(i32),
 }
 
 fn main() {
@@ -68,73 +68,7 @@ fn main() {
 
     match args.command {
         Commands::Maths { equation } => {
-            let split = equation.split(" ").collect::<Vec<&str>>();
-
-            let validated = validate_maths_vector(parse_maths_vector(split));
-
-            let mut result: u32;
-
-            result = if validated.len() == 3 as usize {
-                match (&validated[0], &validated[1], &validated[2]) {
-                    (MathsArg::Int(a), MathsArg::Op(op), MathsArg::Int(b)) => match op {
-                        Operator::Times => a * b,
-                        Operator::Divisor => a / b,
-                        Operator::Minus => a - b,
-                        Operator::Plus => a + b,
-                    },
-                    _ => panic!("Not a valid string"),
-                }
-            } else {
-                let multiplied = multipy_maths_vector(validated);
-                print!("{:?}", multiplied);
-
-                3
-            };
-
-            // let mut result: u32 = 0;
-            // let mut group: (Option<u32>, Option<Operator>, Option<u32>) = (None, None, None);
-
-            // for item in split.into_iter() {
-            //     if group.0 == None {
-            //         match item.parse::<u32>() {
-            //             Ok(res) => group.0 = Some(res),
-            //             Err(err) => panic!("{:?}", err),
-            //         }
-            //     } else if group.1 == None {
-            //         let operator = match item {
-            //             "+" => Some(Operator::Plus),
-            //             "-" => Some(Operator::Minus),
-            //             "*" | "x" => Some(Operator::Times),
-            //             "/" => Some(Operator::Divisor),
-            //             _ => None,
-            //         };
-
-            //         if let Some(op) = operator {
-            //             group.1 = Some(op)
-            //         } else {
-            //             panic!("arguments invalid: no valid operator")
-            //         }
-            //     } else if group.2 == None {
-            //         match item.parse::<u32>() {
-            //             Ok(res) => group.2 = Some(res),
-            //             Err(err) => panic!("{:?}", err),
-            //         }
-
-            //         result = match group {
-            //             (Some(a), Some(operator), Some(b)) => match operator {
-            //                 Operator::Plus => a + b,
-            //                 Operator::Minus => a - b,
-            //                 Operator::Times => a * b,
-            //                 Operator::Divisor => a / b,
-            //             },
-            //             _ => panic!("application failure: arg grouping wasn't done correctly"),
-            //         };
-
-            //         group = (Some(result), None, None)
-            //     }
-            // }
-
-            println!("Result: {}", result);
+            println!("Result: {}", parse_maths_equation(equation));
         }
         Commands::Add {
             first_arg,
@@ -171,7 +105,7 @@ fn parse_maths_vector(vector: Vec<&str>) -> Vec<MathsArg> {
             "-" => MathsArg::Op(Operator::Minus),
             "*" | "x" => MathsArg::Op(Operator::Times),
             "/" => MathsArg::Op(Operator::Divisor),
-            other => match other.parse::<u32>() {
+            other => match other.parse::<i32>() {
                 Ok(res) => MathsArg::Int(res),
                 Err(_) => panic!("couldn't parse value in arg list"),
             },
@@ -207,86 +141,91 @@ fn validate_maths_vector(vector: Vec<MathsArg>) -> Vec<MathsArg> {
     validated
 }
 
-fn multipy_maths_vector(vector: Vec<MathsArg>) -> Vec<MathsArg> {
-    let mut new_vector = Vec::<MathsArg>::new();
-    let mut first_arg = None;
-    let mut second_arg = None;
+fn parse_maths_equation(equation: String) -> i32 {
+    let split = equation.split(" ").collect::<Vec<&str>>();
 
-    let validated_len = vector.len();
+    let validated = validate_maths_vector(parse_maths_vector(split));
 
-    let mut was_mulitplied = false;
-    for (i, third_arg) in vector.into_iter().enumerate() {
-        let new_arg = match (&first_arg, &second_arg, &third_arg) {
-            (Some(MathsArg::Int(a)), Some(MathsArg::Op(op)), MathsArg::Int(b)) => {
-                match op {
-                    Operator::Times => {
-                        if i == 2 {
-                            // new_vector.push(MathsArg::Int(a * b));
-                            was_mulitplied = true;
-                            Some(MathsArg::Int(a * b))
-                        } else if validated_len - 1 == i {
-                            new_vector.push(MathsArg::Int(a * b));
-                            None
-                        } else {
-                            was_mulitplied = true;
-                            Some(MathsArg::Int(a * b))
-                        }
-                    }
-                    Operator::Divisor => {
-                        if i == 2 || was_mulitplied || validated_len - 1 == i {
-                            new_vector.push(MathsArg::Int(*a));
-                            new_vector.push(MathsArg::Op(Operator::Divisor));
-                            new_vector.push(MathsArg::Int(*b));
-                        } else if validated_len - 3 == i {
-                            new_vector.push(MathsArg::Op(Operator::Divisor));
-                        } else {
-                            new_vector.push(MathsArg::Op(Operator::Divisor));
-                            new_vector.push(MathsArg::Int(*b));
-                        }
-                        was_mulitplied = false;
-                        None
-                    }
-                    Operator::Plus => {
-                        if i == 2 || was_mulitplied || validated_len - 1 == i {
-                            new_vector.push(MathsArg::Int(*a));
-                            new_vector.push(MathsArg::Op(Operator::Plus));
-                            new_vector.push(MathsArg::Int(*b));
-                        } else if validated_len - 3 == i {
-                            new_vector.push(MathsArg::Op(Operator::Plus));
-                        } else {
-                            new_vector.push(MathsArg::Op(Operator::Plus));
-                            new_vector.push(MathsArg::Int(*b));
-                        }
-                        was_mulitplied = false;
-                        None
-                    }
-                    Operator::Minus => {
-                        if i == 2 || was_mulitplied || validated_len - 1 == i {
-                            new_vector.push(MathsArg::Int(*a));
-                            new_vector.push(MathsArg::Op(Operator::Minus));
-                            new_vector.push(MathsArg::Int(*b));
-                        } else if validated_len - 3 == i {
-                            new_vector.push(MathsArg::Op(Operator::Minus));
-                        } else {
-                            new_vector.push(MathsArg::Op(Operator::Minus));
-                            new_vector.push(MathsArg::Int(*b));
-                        }
-                        was_mulitplied = false;
-                        None
-                    }
+    let mut result: i32;
+
+    result = if validated.len() == 3 as usize {
+        match (&validated[0], &validated[1], &validated[2]) {
+            (MathsArg::Int(a), MathsArg::Op(op), MathsArg::Int(b)) => match op {
+                Operator::Times => a * b,
+                Operator::Divisor => a / b,
+                Operator::Minus => a - b,
+                Operator::Plus => a + b,
+            },
+            _ => panic!("Not a valid string"),
+        }
+    } else {
+        for arg in validated.into_iter() {}
+
+        9
+    };
+
+    result
+}
+
+fn solve(arr: Vec<MathsArg>) -> i32 {
+    1
+}
+mod tests {
+    mod maths {
+        use crate::{parse_maths_equation, MathsArg, MathsExpectation, Operator};
+
+        #[test]
+        fn handles_any_operator_with_two_args() {
+            use crate::parse_maths_equation;
+
+            struct EquationWithResult {
+                equation: String,
+                result: i32,
+            }
+
+            impl EquationWithResult {
+                fn new(equation: String, result: i32) -> EquationWithResult {
+                    EquationWithResult { equation, result }
                 }
             }
-            _ => None,
-        };
 
-        first_arg = second_arg;
+            let simple_equations = vec![
+                EquationWithResult::new("10 + 2".to_string(), 12),
+                EquationWithResult::new("2 - 1".to_string(), 1),
+                EquationWithResult::new("20 - 1".to_string(), 19),
+                EquationWithResult::new("2 - 10".to_string(), -8),
+                EquationWithResult::new("10 / 2".to_string(), 5),
+                EquationWithResult::new("21 / 7".to_string(), 3),
+                EquationWithResult::new("10 * 2".to_string(), 20),
+                EquationWithResult::new("10 * -2".to_string(), -20),
+            ];
 
-        if let Some(new_arg) = new_arg {
-            second_arg = Some(new_arg)
-        } else {
-            second_arg = Some(third_arg)
+            simple_equations.into_iter().for_each(|test| {
+                assert_eq!(test.result, parse_maths_equation(test.equation));
+            })
+        }
+
+        #[test]
+        fn handles_order_of_ops() {
+            let tests = vec![
+                MathsExpectation {
+                    input: "3 * 3".to_string(),
+                    output: 9,
+                },
+                MathsExpectation {
+                    input: "1 + 5 * 2".to_string(),
+                    output: 11,
+                },
+            ];
+
+            tests.into_iter().for_each(|t| {
+                assert_eq!(parse_maths_equation(t.input), t.output);
+            })
         }
     }
+}
 
-    new_vector
+struct MathsExpectation {
+    input: String,
+    output: i32,
 }
