29d7c204501e625fabbf195c779fa77c489edf58
Ben <benjamin.scottt.dev@gmail.com>
2022-11-08 22:44:56 -0700

    Order of ops (#3)
    * refactor: use pre refactor and validator
    
    pre refactor items to be an Enum allowing us to better handle the vector for future uses
    
    * refactor[validator]: create function
    
    * refactor[parser]: move parser section into fn
    
    * wip: can multiply & divide first
    
    * wip: partial solution to order of ops
    
    * feat: handle multiplication first
    
    * refactor: multiply first into custom fn
    
    * chore: add tests
    
    becoming more tdd driven to help solve for Order of Ops
    
    * refactor: result as non mut
    
    * docs[README]: add development section
    
    * docs[README]: verbosity reduction
    
    * feat[maths]: handle 5 args
    
    * clean: remove linter warnings
    
    * chore: add `Chain` struct & `from` trait to `Operator`
    
    * chore: handle multiplication chain break
    
    * clean: remove pattern catcher
    
    all patterns already caught
    
    * refactor[DRY]: use match | operator
    
    * chore: chain add & multiplication
    
    * fix: end of chain handler
    
    * chore: handle addition chain
    
    * chore: add minus & multiplication hanlding
    
    * chore[Chain]: add prepend property
    
    this is to handle for chains without definite operators between each other
    this will allow to attach - + / * between each chain if needed
    
    * chore: handle divisor & multiplication chains
    
    * chore[Chain]: add calculation method
    
    * chore: parse calculated chains into `MathsArg` vec
    
    * chore: prepend + ops
    
    * chore: prepend chains
    
    refactor: use | in match
    
    * refactor: use `|` for match
    
    * refactor: use `|` match
    
    * test: `chainify`
    
    * feat: handle - & + chains
    
    * test: add all multiplication cases
    
    * test: add plus tests
    
    * test: pass all tests
    
    * chore: handle chain calculations
    
    * chore: handle division
    
    * chore: remove chains
    
    functionality isn't going to work here
    
    * test: add test
    
    * chore: handle multiplication
    
    * chore: handle division and multiplication
    
    * chore: handle addition & subtraction
    
    * clean: remove unused code
    
    * test: add more tests

--- README.md
+++ README.md
@@ -10,7 +10,12 @@ A simple math CLI built with Rust.
 
 `cargo run -- subtract <int> <int>`
 
-## Here's A Visual Reference Of The CLI In Action
+## The CLI In Action
 
 https://user-images.githubusercontent.com/70179217/185538684-baab2fa3-2278-4505-b91e-e788a19489f8.mov
 
+## Development
+
+### Testing
+
+Run `cargo test` to run tests.
--- src/main.rs
+++ src/main.rs
@@ -21,30 +21,30 @@ enum Commands {
     #[clap(arg_required_else_help = true)]
     Add {
         /// first int
-        first_arg: u32,
+        first_arg: i32,
         /// second int
-        second_arg: u32,
+        second_arg: i32,
     },
     #[clap(arg_required_else_help = true)]
     Subtract {
         /// first int
-        first_arg: u32,
+        first_arg: i32,
         /// second int
-        second_arg: u32,
+        second_arg: i32,
     },
     #[clap(arg_required_else_help = true)]
     Multiply {
         /// first int
-        first_arg: u32,
+        first_arg: i32,
         /// second int
-        second_arg: u32,
+        second_arg: i32,
     },
     #[clap(arg_required_else_help = true)]
     Divide {
         /// first int
-        first_arg: u32,
+        first_arg: i32,
         /// second int
-        second_arg: u32,
+        second_arg: i32,
     },
 }
 
@@ -56,57 +56,44 @@ enum Operator {
     Divisor,
 }
 
+impl From<&Operator> for Operator {
+    fn from(op: &Operator) -> Self {
+        match op {
+            Operator::Times => Operator::Times,
+            Operator::Divisor => Operator::Divisor,
+            Operator::Plus => Operator::Plus,
+            Operator::Minus => Operator::Minus,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum MathsArg {
+    Op(Operator),
+    Int(i32),
+}
+
+impl From<&str> for MathsArg {
+    fn from(s: &str) -> Self {
+        match s {
+            "+" => MathsArg::Op(Operator::Plus),
+            "-" => MathsArg::Op(Operator::Minus),
+            "*" => MathsArg::Op(Operator::Times),
+            "/" => MathsArg::Op(Operator::Divisor),
+            s => match s.parse::<i32>() {
+                Ok(n) => MathsArg::Int(n),
+                Err(_) => panic!("not a valid str"),
+            },
+        }
+    }
+}
+
 fn main() {
     let args = Cli::parse();
 
     match args.command {
         Commands::Maths { equation } => {
-            let split = equation.split(" ").collect::<Vec<&str>>();
-
-            let mut result: u32 = 0;
-            let mut group: (Option<u32>, Option<Operator>, Option<u32>) = (None, None, None);
-
-            for item in split.into_iter() {
-                if group.0 == None {
-                    match item.parse::<u32>() {
-                        Ok(res) => group.0 = Some(res),
-                        Err(err) => panic!("{:?}", err),
-                    }
-                } else if group.1 == None {
-                    let operator = match item {
-                        "+" => Some(Operator::Plus),
-                        "-" => Some(Operator::Minus),
-                        "*" | "x" => Some(Operator::Times),
-                        "/" => Some(Operator::Divisor),
-                        _ => None,
-                    };
-
-                    if let Some(op) = operator {
-                        group.1 = Some(op)
-                    } else {
-                        panic!("arguments invalid: no valid operator")
-                    }
-                } else if group.2 == None {
-                    match item.parse::<u32>() {
-                        Ok(res) => group.2 = Some(res),
-                        Err(err) => panic!("{:?}", err),
-                    }
-
-                    result = match group {
-                        (Some(a), Some(operator), Some(b)) => match operator {
-                            Operator::Plus => a + b,
-                            Operator::Minus => a - b,
-                            Operator::Times => a * b,
-                            Operator::Divisor => a / b,
-                        },
-                        _ => panic!("application failure: arg grouping wasn't done correctly"),
-                    };
-
-                    group = (Some(result), None, None)
-                }
-            }
-
-            println!("Result: {}", result);
+            println!("Result: {}", parse_maths_equation(equation));
         }
         Commands::Add {
             first_arg,
@@ -131,6 +118,241 @@ fn main() {
             second_arg,
         } => {
             println!("{}", first_arg / second_arg);
+        }
+    }
+}
+
+fn parse_maths_vector(vector: Vec<&str>) -> Vec<MathsArg> {
+    vector
+        .into_iter()
+        .map(|item| match item {
+            "+" => MathsArg::Op(Operator::Plus),
+            "-" => MathsArg::Op(Operator::Minus),
+            "*" | "x" => MathsArg::Op(Operator::Times),
+            "/" => MathsArg::Op(Operator::Divisor),
+            other => match other.parse::<i32>() {
+                Ok(res) => MathsArg::Int(res),
+                Err(_) => panic!("couldn't parse value in arg list"),
+            },
+        })
+        .collect::<Vec<MathsArg>>()
+}
+
+fn validate_maths_vector(vector: Vec<MathsArg>) -> Vec<MathsArg> {
+    let mut validated = Vec::<MathsArg>::new();
+
+    for (i, item) in vector.into_iter().enumerate() {
+        if i % 2 == 0 {
+            // * Operators should always be at an even index
+            match item {
+                MathsArg::Int(_) => {}
+                MathsArg::Op(_) => panic!("Operators are out of order"),
+            }
+        } else {
+            // * Integers should always be at an even index
+            match item {
+                MathsArg::Op(_) => {}
+                MathsArg::Int(_) => panic!("Integers are out of order"),
+            }
+        }
+
+        validated.push(item)
+    }
+
+    if validated.len() % 2 == 0 {
+        panic!("Even number of arguments")
+    }
+
+    validated
+}
+
+fn parse_maths_equation(equation: String) -> i32 {
+    let split = equation.split(" ").collect::<Vec<&str>>();
+
+    let mut validated = validate_maths_vector(parse_maths_vector(split));
+
+    let result = if validated.len() == 3 {
+        match (&validated[0], &validated[1], &validated[2]) {
+            (MathsArg::Int(a), MathsArg::Op(op), MathsArg::Int(b)) => match op {
+                Operator::Times => a * b,
+                Operator::Divisor => a / b,
+                Operator::Minus => a - b,
+                Operator::Plus => a + b,
+            },
+            _ => panic!("Not a valid string"),
+        }
+    } else if validated.len() == 5 {
+        match (
+            &validated[0],
+            &validated[1],
+            &validated[2],
+            &validated[3],
+            &validated[4],
+        ) {
+            (
+                MathsArg::Int(a),
+                MathsArg::Op(op_a),
+                MathsArg::Int(b),
+                MathsArg::Op(op_b),
+                MathsArg::Int(c),
+            ) => match (op_a, op_b) {
+                (Operator::Times, Operator::Times) => a * b * c,
+                (Operator::Times, Operator::Divisor) => a * b / c,
+                (Operator::Times, Operator::Plus) => (a * b) + c,
+                (Operator::Times, Operator::Minus) => (a * b) - c,
+                (Operator::Divisor, Operator::Times) => a / b * c,
+                (Operator::Divisor, Operator::Divisor) => a / b / c,
+                (Operator::Divisor, Operator::Plus) => (a / b) + c,
+                (Operator::Divisor, Operator::Minus) => (a / b) - c,
+                (Operator::Plus, Operator::Times) => a + (b * c),
+                (Operator::Plus, Operator::Divisor) => a + (b / c),
+                (Operator::Plus, Operator::Plus) => a + b + c,
+                (Operator::Plus, Operator::Minus) => a + b - c,
+                (Operator::Minus, Operator::Times) => a - (b * c),
+                (Operator::Minus, Operator::Divisor) => a - (b / c),
+                (Operator::Minus, Operator::Plus) => a - b + c,
+                (Operator::Minus, Operator::Minus) => a - b - c,
+            },
+            _ => panic!("Not a valid string!"),
+        }
+    } else {
+        while validated.contains(&MathsArg::Op(Operator::Times))
+            || validated.contains(&MathsArg::Op(Operator::Divisor))
+        {
+            let idx = validated.iter().position(|arg| match arg {
+                MathsArg::Op(Operator::Times) => true,
+                MathsArg::Op(Operator::Divisor) => true,
+                _ => false,
+            });
+
+            let idx = idx.expect("Expecting an item");
+
+            let x = &validated[idx - 1];
+            let y = &validated[idx + 1];
+
+            let (x, y) = match (x, y) {
+                (MathsArg::Int(x), MathsArg::Int(y)) => (*x, *y),
+                _ => panic!("should be integers"),
+            };
+
+            let res: i32 = match &validated[idx] {
+                MathsArg::Op(Operator::Times) => x * y,
+                MathsArg::Op(Operator::Divisor) => x / y,
+                _ => panic!("shouldn't be anything other than multiplication or division"),
+            };
+
+            validated[idx + 1] = MathsArg::Int(res);
+            validated.remove(idx);
+            validated.remove(idx - 1);
+        }
+
+        while validated.contains(&MathsArg::Op(Operator::Plus))
+            || validated.contains(&MathsArg::Op(Operator::Minus))
+        {
+            let idx = validated.iter().position(|arg| match arg {
+                MathsArg::Op(Operator::Plus) => true,
+                MathsArg::Op(Operator::Minus) => true,
+                _ => false,
+            });
+
+            let idx = idx.expect("Expecting an item");
+
+            let x = &validated[idx - 1];
+            let y = &validated[idx + 1];
+
+            let (x, y) = match (x, y) {
+                (MathsArg::Int(x), MathsArg::Int(y)) => (*x, *y),
+                _ => panic!("should be integers"),
+            };
+
+            let res: i32 = match &validated[idx] {
+                MathsArg::Op(Operator::Plus) => x + y,
+                MathsArg::Op(Operator::Minus) => x - y,
+                _ => panic!("shouldn't be anything other than addition or subtraction"),
+            };
+
+            validated[idx + 1] = MathsArg::Int(res);
+            validated.remove(idx);
+            validated.remove(idx - 1);
+        }
+
+        match validated[0] {
+            MathsArg::Int(res) => res,
+            _ => panic!("should result in an integer"),
+        }
+    };
+
+    result
+}
+
+mod tests {
+    mod maths {
+        #[test]
+        fn handles_any_operator_with_two_args() {
+            use crate::parse_maths_equation;
+
+            struct EquationWithResult {
+                equation: String,
+                result: i32,
+            }
+
+            impl EquationWithResult {
+                fn new(equation: String, result: i32) -> EquationWithResult {
+                    EquationWithResult { equation, result }
+                }
+            }
+
+            let simple_equations = vec![
+                EquationWithResult::new("10 + 2".to_string(), 12),
+                EquationWithResult::new("2 - 1".to_string(), 1),
+                EquationWithResult::new("20 - 1".to_string(), 19),
+                EquationWithResult::new("2 - 10".to_string(), -8),
+                EquationWithResult::new("10 / 2".to_string(), 5),
+                EquationWithResult::new("21 / 7".to_string(), 3),
+                EquationWithResult::new("10 * 2".to_string(), 20),
+                EquationWithResult::new("10 * -2".to_string(), -20),
+            ];
+
+            simple_equations.into_iter().for_each(|test| {
+                assert_eq!(test.result, parse_maths_equation(test.equation));
+            })
+        }
+
+        #[test]
+        fn handles_order_of_ops() {
+            use crate::parse_maths_equation;
+
+            struct MathsExpectation {
+                input: String,
+                output: i32,
+            }
+
+            let tests = vec![
+                MathsExpectation {
+                    input: "3 * 3".to_string(),
+                    output: 9,
+                },
+                MathsExpectation {
+                    input: "1 + 5 * 2".to_string(),
+                    output: 11,
+                },
+                MathsExpectation {
+                    input: "2 + 8 - 4 * 3 / 2".to_string(),
+                    output: 4,
+                },
+                MathsExpectation {
+                    input: "10 / 2 * 2".to_string(),
+                    output: 10,
+                },
+                MathsExpectation {
+                    input: "15 * 3 - 2 * 20 + 10 / 2 - 5".to_string(),
+                    output: 5,
+                },
+            ];
+
+            tests.into_iter().for_each(|t| {
+                assert_eq!(parse_maths_equation(t.input), t.output);
+            })
         }
     }
 }
